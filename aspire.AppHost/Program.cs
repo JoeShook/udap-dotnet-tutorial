0ca6491ac8aab93c8d8ef94407530507f36144f5
Joseph Shook <Joseph.Shook@Surescripts.com>
2025-03-11 06:22:48 -0700

    fixups
    UdapEd-Tutorial container build from Dockerfile.UdapEd to trust my generated CA.
    Include udap-tutorial-dev-tls-cert.pfx TLS cert I generated for convenience.   Might make the user generate this in the future.  But not sure yet.  It is nice to just have it.

--- aspire.AppHost/Program.cs
+++ aspire.AppHost/Program.cs
@@ -11,11 +11,13 @@ builder.AddProject<Projects.udap_idp_server_devdays>("udap-idp-server-devdays");
 var userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 var emrDirectClientPasscode = builder.Configuration["sampleKeyC"]; // Projects UserSecretsId to point to secrets.json
 
-builder.AddContainer("UdapEd-Tutorial", "ghcr.io/joeshook/udaped", "v0.4.4.30")
+builder.AddDockerfile("UdapEd-Tutorial", "..", "Dockerfile.UdapEd") // used to be the image, ghcr.io/joeshook/udaped:v0.4.4.30
+                                                                    // but need to add a trusted CA.
     .WithContainerName("UdapEd-Tutorial")
+    .WithImageTag("latest")
     .WithHttpsEndpoint(port: 7041, targetPort: 8181)
     .WithEnvironment("ASPNETCORE_HTTPS_PORTS", "8181")
-    .WithEnvironment("ASPNETCORE_Kestrel__Certificates__Default__Password", "password")
+    .WithEnvironment("ASPNETCORE_Kestrel__Certificates__Default__Password", "udap-test")
     .WithEnvironment("ASPNETCORE_Kestrel__Certificates__Default__Path", "/home/app/.aspnet/https/udap-tutorial-dev-tls-cert.pfx")
     .WithEnvironment("sampleKeyC", emrDirectClientPasscode)
     .WithBindMount("../CertificateStore", "/home/app/.aspnet/https", true);
